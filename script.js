8926fc33798996db025f1ae236a7ba1e3c228867
Vadim Moroz <vmoroz13@yandex.ru>
2020-06-08 09:03:55 +0300

    Исправил выыод строкой и числом

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let num = 266219 ;
 var arr = num.toString().split('') ;
 
 
-
    let res = arr.reduce((sum, key) => sum  * key); 
     console.log(res);
 
@@ -11,6 +10,16 @@ let sqr = res ** 3;
 
     console.log(sqr);
 
-    document.write(sqr.toString().split('').slice(0, 2));
-   
+    //document.write(sqr.toString().split('').slice(0, 2).join());
+let twoArrey = sqr.toString().split('').slice(0, 2);
 
+let str = String(twoArrey);// строка
+document.write(str);
+
+twoArrey.sort((a, b)=>{
+    console.log (typeof(a, b));  //строка
+});
+
+twoArrey.forEach((item)=>{
+    document.write(Number(item)); //number
+})
