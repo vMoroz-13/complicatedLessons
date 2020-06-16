eb30b89e52def9c3d3244ab46525cc38cd60b7bc
Vadim Moroz <vmoroz13@yandex.ru>
2020-06-16 16:15:17 +0300

    урок 9-й усложненное 2+ 1баллов)))

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 'use strict';
-let arr = ['452', '13', '205', '104', '47', '748', '1377']; // в виде строк
+const date = document.querySelector('.date');
+var d = new Date();
+var c = d.getHours();
+var y = d.getMinutes();
+var k = d.getSeconds();
+ 
+var day=new Array("Воскресенье","Понедельник","Вторник",
+"Среда","Четверг","Пятница","Суббота");
+ 
+var month=new Array("января","февраля","марта","апреля","мая","июня",
+"июля","августа","сентября","октября","ноября","декабря");
 
-arr.forEach((item) => {
-  if (item.startsWith('2') || item.startsWith('4')) {
-    console.log(item );
-  }
-});
-
-mark:
-for (let i = 2; i <= 100; i++) { 
-
-	for (let j = 2; j < i; j++) { 
-	  if (i % j === 0) continue mark; // не подходит, берём следующее
-	}
-  
-	console.log( i + ' -Делители этого числа: 1 и ' + i); 
-  }
+let res ="'а) Сегодня " + day[d.getDay()]+", " +d.getDate()+ " " + month[d.getMonth()]
++ " " + d.getFullYear() + " года.," + c +' час ' + y + ' мин '+ k + " секунд' ";
 
 
+var Year = d.getFullYear();
+var Month = d.getMonth()+1;
+var Day = d.getDate();
+
+let res2 = "'б)"+ Day + "." + Month + "." + Year + '-' + c +':'+ y + ':' + k+"'";
+
+date.style.color = 'rgb(255, 0, 0)';
+date.style.fontSize = '14pt ';
+date.style.fontFamily = 'Roboto, sans-serif';
+date.style.fontWeight = 'bold';
+date.innerHTML = res + '<Br><Br>' + res2;
+
+//let timerId = setInterval(() => date.innerHTML =  , 2000);
+var time = setInterval(function() {
+  var date = new Date();
+  document.getElementById("time").innerHTML = (date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
+}, 1000);
